2f773c218b502d173156175a12e7743654e0b6a1
KrzysztofKrupicki <147048678+KrzysztofKrupicki@users.noreply.github.com>
2025-01-12 13:28:27 +0100

    dodano najwiekszy wspolny podciag

---
+++ najwiekszyWspolnyPodciag_08_01/NajwiekszyWspolnyPodciag.cs
@@ -0,0 +1,131 @@
+﻿namespace najwiekszyWspolnyPodciag_08_01
+{
+    internal class NajwiekszyWspolnyPodciag
+    {
+        private String string_1;
+        private String string_2;
+        private int[,] matrix;
+
+        public NajwiekszyWspolnyPodciag(String string_1, String string_2)
+        {
+            this.string_1 = string_1;
+            this.string_2 = string_2;
+            this.matrix = GenerujMacierz();
+        }
+
+        public int[,] GenerujMacierz()
+        {
+            string s1 = " " + string_1;
+            string s2 = " " + string_2;
+            int[,] matrix = new int[s1.Length, s2.Length];
+            for (int i = 1; i < s1.Length; i++)
+            {
+                for (int j = 1; j < s2.Length; j++)
+                {
+                    if (s1[i] != s2[j])
+                    {
+                        matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i, j - 1]);
+                    }
+                    else
+                    {
+                        matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        public void WyswietlMacierz()
+        {
+            Console.WriteLine("Macierz:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void WyswietlMacierzZLiterami()
+        {
+            string s1 = " " + string_1;
+            string s2 = " " + string_2;
+            Console.WriteLine("Macierz z literami:");
+            Console.Write("  ");
+            for (int j = 0; j < s2.Length; j++)
+            {
+                Console.Write(s2[j] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write(s1[i] + " ");
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int Dlugosc()
+        {
+            int i = matrix.GetLength(0) - 1;
+            int j = matrix.GetLength(1) - 1;
+            return matrix[i, j];
+        }
+
+        public string PodciagGora()
+        {
+            string podciag = "";
+            int i = matrix.GetLength(0) - 1;
+            int j = matrix.GetLength(1) - 1;
+            while (i > 0 && j > 0)
+            {
+                if (matrix[i - 1, j] == matrix[i, j])
+                {
+                    i--;
+                }
+                else if (matrix[i, j - 1] == matrix[i, j])
+                {
+                    j--;
+                }
+                else
+                {
+                    podciag = string_1[i - 1] + podciag;
+                    i--;
+                    j--;
+                }
+            }
+            return podciag;
+        }
+
+        public string PodciagLewo()
+        {
+            string podciag = "";
+            int i = matrix.GetLength(0) - 1;
+            int j = matrix.GetLength(1) - 1;
+            while (i > 0 && j > 0)
+            {
+                if (matrix[i, j - 1] == matrix[i, j])
+                {
+                    j--;
+                }
+                else if (matrix[i - 1, j] == matrix[i, j])
+                {
+                    i--;
+                }
+                else
+                {
+                    podciag = string_1[i - 1] + podciag;
+                    i--;
+                    j--;
+                }
+            }
+            return podciag;
+        }
+    }
+}
---
+++ najwiekszyWspolnyPodciag_08_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using najwiekszyWspolnyPodciag_08_01;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "babab";
+        string s2 = "abaabbaaa";
+        string s3 = "KALIMALBA";
+        string s4 = "ALIBABA";
+        NajwiekszyWspolnyPodciag NWP = new NajwiekszyWspolnyPodciag(s1, s2);
+        NWP.WyswietlMacierzZLiterami();
+        Console.WriteLine("Długość najwiekszego wspólnego podciągu: " + NWP.Dlugosc());
+        Console.WriteLine("Podciąg góra: " + NWP.PodciagGora());
+        Console.WriteLine("Podciąg lewo: " + NWP.PodciagLewo());
+    }
+}
